1cefbe2c5bf24537655b04c9d2804630b9c38eb0
alekseenkov.ilya <alexeenkov-frontend@yandex.ru>
2024-11-21 20:26:59 +0300

    Добавление логики на доску задач в проекте Vue-Work

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app_layout">
+    <AppLayoutHeader />
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader.vue";
+</script>
+
+<style lang="scss" scoped>
+.app_layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
